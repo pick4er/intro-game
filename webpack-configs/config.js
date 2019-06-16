2c3a5f11b7efd6997b990990511cbc0d002a29c6
pick4er <pick4er@gmail.com>
2019-06-16 15:54:24 +0300

    Initial webpack and folders structure setup

---
+++ webpack-configs/config.js
@@ -0,0 +1,38 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = function configWebpack(props) {
+  const { production = true } = props;
+
+  return {
+    mode: production ? 'production' : 'development',
+    entry: {
+      index: './src/client/index.js',
+    },
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, '..', 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'src/client/index.html',
+      }),
+    ],
+  };
+};
